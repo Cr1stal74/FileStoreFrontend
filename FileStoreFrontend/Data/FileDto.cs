65fdb7bd5181f428bb8a49f16708ef9802190095
cr1stal <cr1stal@DESKTOP-C3QV6H8>
2023-10-08 23:06:15 +0300

    Добавьте файлы проекта.

---
+++ FileStoreFrontend/Data/FileDto.cs
@@ -0,0 +1,11 @@
+﻿namespace FileStoreFrontend.Data;
+
+public class FileDto
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public long Size { get; set; }
+    public bool IsInDatabase { get; set; }
+    public bool IsDone { get; set; }
+}
+
